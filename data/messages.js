e1371ba777f6ba227c21a67114d30017ccc484d4
LG972021 <liamgill97@hotmail.com>
2021-10-26 08:54:07 +0100

    Add initial  Data - Just in case needed

---
+++ data/messages.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    author: "NC",
+    attachments: null,
+    message_body: "This is message 1",
+    created_at: new Date(1610964020514),
+  },
+  {
+    author: "NC",
+    attachments: null,
+    message_body: "This is message 2",
+    created_at: new Date(1610964020514),
+  },
+  {
+    author: "NC",
+    attachments: null,
+    message_body: "This is message 3",
+    created_at: new Date(1610964020514),
+  },
+];
---
+++ data/users.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    username: "AS",
+    name: "Andy Smith",
+    avatar_url:
+      "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+    online: false,
+  },
+  {
+    username: "CP",
+    name: "Carrie Platten",
+    avatar_url:
+      "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+    online: false,
+  },
+  {
+    username: "LG",
+    name: "Liam Gill ",
+    avatar_url:
+      "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+    online: false,
+  },
+  {
+    username: "SD",
+    name: "Samuel Dick",
+    avatar_url:
+      "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+    online: false,
+  },
+  {
+    username: "NC",
+    name: "North Coder",
+    avatar_url:
+      "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+    online: false,
+  },
+];
