b98591f8ba4cc0b04a1c07af954d4ce5f1430bd3
samualdi <samualdick89@gmail.com>
2021-10-26 08:49:11 +0100

    adapting server with test html

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const path = require('path');
 
 const app = express();
 const port = 3000
+
+
+ const publicDirectoryPath = path.join(__dirname,'/public');
+
+app.use(express.static(publicDirectoryPath));
+
+
 app.listen(port, () => {
     console.log('server is running on 3000!')
 })
 
-// app.get('/', );
+
 
 
 
