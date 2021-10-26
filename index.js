fa78b7b920a8c525a1e71454caac7eec328b7e41
samualdi <samualdick89@gmail.com>
2021-10-26 12:11:04 +0100

    Add socket.io functionality to express server and render chat form in index.html

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
-const port = 3000
+const server = http.createServer(app);
+const io = socketio(server);
+const port = 8991
 
 
- const publicDirectoryPath = path.join(__dirname,'/public');
+ const publicDirectoryPath = path.join(__dirname,'/public'); 
 
 app.use(express.static(publicDirectoryPath));
 
+io.on('connection', (client) => {
+    console.log(`New websocket connection -> ${client.id}` );
+ client.on("messageFromClient", (msg) => {
+   io.emit("messageFromServer", msg);
+ });
+  client.on('disconnect', () => {
+    console.log('New websocket disconnected');
+  });
+});
 
-app.listen(port, () => {
-    console.log('server is running on 3000!')
-})
+server.listen(port,()=>{
+    console.log(`Server is up on port ${port}!`);
+}
+)
+
+
 
 
 
